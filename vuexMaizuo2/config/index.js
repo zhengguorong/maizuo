b48f1994030b5c2f5f17a1a34d3a99e99c07e083
郑国榕 <zhengguorong@bluemoon.com.cn>
2017-01-12 09:04:21 +0800

    解决跨域问题

--- vuexMaizuo2/config/index.js
+++ vuexMaizuo2/config/index.js
@@ -21,7 +21,15 @@ module.exports = {
     port: 8080,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/v4': {
+        target: 'http://m.maizuo.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/v4': '/v4'
+        }
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
