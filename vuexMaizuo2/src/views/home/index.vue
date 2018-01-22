bedf7b16684cad81f7d5febef01f08656d37fbb1
郑国榕 <zhengguorong@bluemoon.com.cn>
2018-01-22 14:51:35 +0800

    修复swiper报错

--- vuexMaizuo2/src/views/home/index.vue
+++ vuexMaizuo2/src/views/home/index.vue
@@ -16,48 +16,55 @@
 
 </template>
 <style lang="less">
+.my-swipe {
+  height: 9rem;
+  color: #ffffff;
+  font-size: 30px;
+  text-align: center;
+  overflow: hidden;
+}
+.silde img {
+  width: 100%;
+}
 
-  .my-swipe {
-    height: 9rem;
-    color: #ffffff;
-    font-size: 30px;
-    text-align: center;
-    overflow: hidden;
-  }
-  .silde img {
-    width: 100%;
-  }
-
-  .movie ul {
-    padding-top: 18px;
-  }
+.movie ul {
+  padding-top: 18px;
+}
 </style>
 <script>
-  import NowPlaying from './now-playing'
-  import ComingSoon from './coming-soon'
-  import ImagePlaceholder from '../../components/image-placeholder'
-  import { swiper, swiperSlide } from 'vue-awesome-swiper'
-  import { mapGetters } from 'vuex'
+import NowPlaying from './now-playing'
+import ComingSoon from './coming-soon'
+import ImagePlaceholder from '../../components/image-placeholder'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import { mapGetters } from 'vuex'
 
-  export default{
-    computed: {
+export default {
+  data () {
+    return {
       swiperOption: {
         autoplay: 3000,
         autoHeight: true
-      },
-      ...mapGetters({
-        comingSoonFilms: 'getComingSoonFilms',
-        nowPlayingFilms: 'getNowPlayingFilms',
-        billboards: 'getBillboards'
-      })
-    },
-    created () {
-      this.$store.dispatch('fetchComingSoonLists', 1, 5)
-      this.$store.dispatch('fetchNowPlayingLists', 1, 5)
-      this.$store.dispatch('fetchBillboards')
-    },
-    components: {
-      NowPlaying, ComingSoon, ImagePlaceholder, swiper, swiperSlide
+      }
     }
+  },
+  computed: {
+    ...mapGetters({
+      comingSoonFilms: 'getComingSoonFilms',
+      nowPlayingFilms: 'getNowPlayingFilms',
+      billboards: 'getBillboards'
+    })
+  },
+  created () {
+    this.$store.dispatch('fetchComingSoonLists', 1, 5)
+    this.$store.dispatch('fetchNowPlayingLists', 1, 5)
+    this.$store.dispatch('fetchBillboards')
+  },
+  components: {
+    NowPlaying,
+    ComingSoon,
+    ImagePlaceholder,
+    swiper,
+    swiperSlide
   }
+}
 </script>
